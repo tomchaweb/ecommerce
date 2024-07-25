89baa235c5963b61061c46141c383848933804d9
Tom Chapman <tomcha.web@gmail.com>
2024-07-25 10:22:42 +0100

    add details to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,37 @@ import Footer from "../components/Footer";
 export default function About() {
   return (
     <>
-      <div className="about">
-        <h1>About Us</h1>
-        <p>some about text</p>
+      <div className="about-page">
+        <section className="about-header">
+          <h1>Get to know us</h1>
+        </section>
+        <section className="about-main">
+          <div className="about-us-text-box">
+            <h2>Our Story</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
+              ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus
+              et magnis dis parturient montes nascetur ridiculus mus. Vestibulum
+              ultricies aliquam convallis.
+            </p>
+          </div>
+          <img></img>
+          <img></img>
+          <div className="about-us-text-box">
+            <h2>Our Vision</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
+              ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus
+              et magnis dis parturient montes nascetur ridiculus mus. Vestibulum
+              ultricies aliquam convallis.
+            </p>
+          </div>
+        </section>
+        <section className="about-key-points">
+          <div className="about-key-points-card"></div>
+          <div className="about-key-points-card"></div>
+          <div className="about-key-points-card"></div>
+        </section>
       </div>
       <Footer />
     </>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 import data from "../data";
 
@@ -58,7 +58,7 @@ export default function Products() {
         break;
       case "reset":
         setProducts(data);
-        setActiveFilters([]);
+        // setActiveFilters([]);
         break;
       default:
         break;
