a8aebecc677e5e8d0f52bd9381534546987df73c
Tom Chapman <tomcha.web@gmail.com>
2024-07-29 16:10:23 +0100

    add styling to filter buttons on product page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,27 +9,8 @@ import ScrollToTop from "../components/ScrollToTop";
 
 export default function Products() {
   const [products, setProducts] = useState(data);
-  // const [isMobile, setIsMobile] = useState(true);
-
-  // function handleResize() {
-  //   if (window.innerWidth < 750) {
-  //     setIsMobile(true);
-  //   } else {
-  //     setIsMobile(false);
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   if (window.innerWidth > 750) {
-  //     setIsMobile(false);
-  //   } else {
-  //     setIsMobile(true);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   window.addEventListener("resize", handleResize);
-  // });
+  const [activeFilter, setActiveFilter] = useState("");
+  const [activeSort, setActiveSort] = useState("");
 
   function filterProducts(e) {
     let { filter } = e.target.dataset;
@@ -39,21 +20,24 @@ export default function Products() {
         setProducts(
           data.filter((item) => {
             return item.price > 50 && item.price <= 90;
-          })
+          }),
+          setActiveFilter("lessThan90")
         );
         break;
       case "lessThan50":
         setProducts(
           data.filter((item) => {
             return item.price > 30 && item.price <= 50;
-          })
+          }),
+          setActiveFilter("lessThan50")
         );
         break;
       case "lessThan30":
         setProducts(
           data.filter((item) => {
             return item.price > 0 && item.price <= 30;
-          })
+          }),
+          setActiveFilter("lessThan30")
         );
         break;
       case "reset":
@@ -73,11 +57,13 @@ export default function Products() {
         return a.price - b.price;
       });
       setProducts(sortedProducts);
+      setActiveSort("price");
     } else if (sort === "priceReverse") {
       let sortedProducts = [...products].sort(function (a, b) {
         return b.price - a.price;
       });
       setProducts(sortedProducts);
+      setActiveSort("priceReverse");
     } else if (sort === "name") {
       let sortedProducts = [...products].sort(function (a, b) {
         let x = a.name.toLowerCase();
@@ -91,11 +77,13 @@ export default function Products() {
         return 0;
       });
       setProducts(sortedProducts);
+      setActiveSort("name");
     } else if (sort === "rating") {
       let sortedProducts = [...products].sort(function (a, b) {
         return b.rating - a.rating;
       });
       setProducts(sortedProducts);
+      setActiveSort("rating");
     }
   }
 
@@ -106,28 +94,56 @@ export default function Products() {
         <div className="buttons">
           <div className="filter-buttons">
             <h2>Price Range</h2>
-            <button onClick={filterProducts} data-filter="lessThan90">
+            <button
+              onClick={filterProducts}
+              data-filter="lessThan90"
+              className={activeFilter === "lessThan90" ? "active-filter" : null}
+            >
               £50 - £90
             </button>
-            <button onClick={filterProducts} data-filter="lessThan50">
+            <button
+              onClick={filterProducts}
+              data-filter="lessThan50"
+              className={activeFilter === "lessThan50" ? "active-filter" : null}
+            >
               £30 - £50
             </button>
-            <button onClick={filterProducts} data-filter="lessThan30">
+            <button
+              onClick={filterProducts}
+              data-filter="lessThan30"
+              className={activeFilter === "lessThan30" ? "active-filter" : null}
+            >
               £0 - £30
             </button>
           </div>
           <div className="sort-buttons">
             <h2>Sort by</h2>
-            <button onClick={sortProducts} data-sort="price">
+            <button
+              onClick={sortProducts}
+              data-sort="price"
+              className={activeSort === "price" ? "active-sort" : null}
+            >
               price: low to high
             </button>
-            <button onClick={sortProducts} data-sort="priceReverse">
+            <button
+              onClick={sortProducts}
+              data-sort="priceReverse"
+              className={activeSort === "priceReverse" ? "active-sort" : null}
+            >
               price: high to low
             </button>
-            <button onClick={sortProducts} data-sort="name">
+            <button
+              onClick={sortProducts}
+              data-sort="name"
+              className={activeSort === "name" ? "active-sort" : null}
+            >
               name: A-Z
             </button>
-            <button onClick={sortProducts} data-sort="rating">
+            <button
+              onClick={sortProducts}
+              data-sort="rating"
+              className={activeSort === "rating" ? "active-sort" : null}
+            >
               rating
             </button>
           </div>
